14b42b18475de28c2c88fb7f1ffb33ceb107721f
computervisionlearner <33013384+computervisionlearner@users.noreply.github.com>
2017-12-12 11:40:10 +0800

    如何从ckpt中获取变量

---
+++ variable_or_train_method/get_variable_from_ckpt.py
@@ -0,0 +1,7 @@
+from tensorflow.python import pywrap_tensorflow  
+checkpoint_path = os.path.join(model_dir, "model.ckpt")  
+reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)  
+var_to_shape_map = reader.get_variable_to_shape_map()  
+for key in var_to_shape_map:  
+    print("tensor_name: ", key)  
+    print(reader.get_tensor(key))
