caccc42235d7d46b939b02344e33cf78a14462ee
computervisionlearner <33013384+computervisionlearner@users.noreply.github.com>
2018-12-26 11:30:01 +0800

    Create mnist.py

---
+++ example_of_BN/mnist.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 24 20:35:12 2017
+
+@author: no1
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+
+import tensorflow as tf
+
+# The MNIST dataset has 10 classes, representing the digits 0 through 9.
+NUM_CLASSES = 10
+
+# The MNIST images are always 28x28 pixels.
+IMAGE_SIZE = 28
+IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
+
+def batch_norm(x, n_out, phase_train, scope='bn'):
+    """
+    Batch normalization on convolutional maps.
+    Args:
+        x:           Tensor, 4D BHWD input maps
+        n_out:       integer, depth of input maps
+        phase_train: boolean tf.Varialbe, true indicates training phase
+        scope:       string, variable scope
+    Return:
+        normed:      batch-normalized maps
+    """
+    with tf.variable_scope(scope):
+        beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
+                                     name='beta', trainable=True)
+        gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
+                                      name='gamma', trainable=True)
+        batch_mean, batch_var = tf.nn.moments(x, [0], name='moments')
+        ema = tf.train.ExponentialMovingAverage(decay=0.99)
+
+        def mean_var_with_update():
+            ema_apply_op = ema.apply([batch_mean, batch_var])
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(batch_mean), tf.identity(batch_var)
+
+        mean, var = tf.cond(phase_train,
+                            mean_var_with_update,
+                            lambda: (ema.average(batch_mean), ema.average(batch_var)))
+        normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+    return normed
+  
+def inference(images, hidden1_units, hidden2_units, is_training):
+  """Build the MNIST model up to where it may be used for inference.
+
+  Args:
+    images: Images placeholder, from inputs().
+    hidden1_units: Size of the first hidden layer.
+    hidden2_units: Size of the second hidden layer.
+
+  Returns:
+    softmax_linear: Output tensor with the computed logits.
+  """
+  # Hidden 1
+  with tf.name_scope('hidden1'):
+    weights = tf.Variable(
+        tf.truncated_normal([IMAGE_PIXELS, hidden1_units],
+                            stddev=1.0 / math.sqrt(float(IMAGE_PIXELS))),
+        name='weights')
+    biases = tf.Variable(tf.zeros([hidden1_units]),
+                         name='biases')
+    conv1 = tf.matmul(images, weights) + biases
+#    bn1 = tf.layers.batch_normalization(conv1, training = is_training)
+    bn1 = tf.contrib.layers.batch_norm(conv1,is_training=is_training)
+#    bn1 = batch_norm(conv1, hidden1_units, is_training, "bn1")
+    hidden1 = tf.nn.relu(bn1)
+  # Hidden 2
+  with tf.name_scope('hidden2'):
+    weights = tf.Variable(
+        tf.truncated_normal([hidden1_units, hidden2_units],
+                            stddev=1.0 / math.sqrt(float(hidden1_units))),
+        name='weights')
+    biases = tf.Variable(tf.zeros([hidden2_units]),
+                         name='biases')
+    
+    conv2 = tf.matmul(hidden1, weights) + biases
+#    bn2 = tf.layers.batch_normalization(conv2, training = is_training)
+    bn2 = tf.contrib.layers.batch_norm(conv2,is_training=is_training)
+#    bn2 = batch_norm(conv2, hidden2_units, is_training, "bn2")
+    hidden2 = tf.nn.relu(bn2)
+  # Linear
+  with tf.name_scope('softmax_linear'):
+    weights = tf.Variable(
+        tf.truncated_normal([hidden2_units, NUM_CLASSES],
+                            stddev=1.0 / math.sqrt(float(hidden2_units))),
+        name='weights')
+    biases = tf.Variable(tf.zeros([NUM_CLASSES]),
+                         name='biases')
+    logits = tf.matmul(hidden2, weights) + biases
+  return logits
+
+
+def loss(logits, labels):
+  """Calculates the loss from the logits and the labels.
+
+  Args:
+    logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+    labels: Labels tensor, int32 - [batch_size].
+
+  Returns:
+    loss: Loss tensor of type float.
+  """
+  labels = tf.to_int64(labels)
+#  labels = tf.to_float(labels)
+  cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits, name='xentropy')
+#  y_conv = tf.nn.softmax(logits)
+#  cross_entropy = -tf.reduce_sum(labels*tf.log(y_conv))
+  return tf.reduce_mean(cross_entropy, name='xentropy_mean')
+
+
+def training(loss):
+  """Sets up the training Ops.
+
+  Creates a summarizer to track the loss over time in TensorBoard.
+
+  Creates an optimizer and applies the gradients to all trainable variables.
+
+  The Op returned by this function is what must be passed to the
+  `sess.run()` call to cause the model to train.
+
+  Args:
+    loss: Loss tensor, from loss().
+    learning_rate: The learning rate to use for gradient descent.
+
+  Returns:
+    train_op: The Op for training.
+  """
+  # Add a scalar summary for the snapshot loss.
+  
+  update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+  optimizer = tf.train.AdamOptimizer()
+  with tf.control_dependencies(update_ops):
+    train_op = optimizer.minimize(loss) 
+  return train_op
+
+
+def evaluation(logits, labels):
+  """Evaluate the quality of the logits at predicting the label.
+
+  Args:
+    logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+    labels: Labels tensor, int32 - [batch_size], with values in the
+      range [0, NUM_CLASSES).
+
+  Returns:
+    A scalar int32 tensor with the number of examples (out of batch_size)
+    that were predicted correctly.
+  """
+  # For a classifier model, we can use the in_top_k Op.
+  # It returns a bool tensor with shape [batch_size] that is true for
+  # the examples where the label is in the top k (here k=1)
+  # of all logits for that example.
+  correct = tf.nn.in_top_k(logits, labels, 1)
+  # Return the number of true entries.
+  return tf.reduce_sum(tf.cast(correct, tf.int32))
